cb0ffcea403021812668bda3307858fa2e50d8ef
helengao027 <helen.gao@berkeley.edu>
2016-07-05 14:57:51 -0400

    initial commit

---
+++ Assignment2/Assignment2/MainController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by H & E on 7/5/16.
+//  Copyright © 2016 H & E. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+class Problem2ViewController: UIViewController {
+    
+    @IBAction func buttonClicked(sender: UIButton) {
+    }
+   
+    @IBOutlet weak var textShown: UITextView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Problem 2"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+class Problem3ViewController: UIViewController {
+    
+    @IBAction func buttonClicked(sender: UIButton) {
+    }
+    @IBOutlet weak var textShown: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Problem 3"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+class Problem4ViewController: UIViewController {
+    
+    @IBAction func buttonClicked(sender: UIButton) {
+    }
+    @IBOutlet weak var textShown: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Problem 4"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
